0b300c210cd51d141cb55f519e31ba7fab4d9921
ameridass <ameridas1998@gmail.com>
2024-04-08 13:44:47 -0600

    feat
    Serializer para modelo de propietaros (duenos).

---
+++ veterinaria/veterinaria_api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Propietario
+
+
+class PropietarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Propietario
+        fields = '__all__'
