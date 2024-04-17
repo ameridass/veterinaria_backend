0d0533df56ddb0b7a5a09a13097942064897c638
asosa <asosa@gtinfile.onmicrosoft.com>
2024-04-17 14:25:52 -0600

    feat
    Ruteo de consultas (desparacitaciones).

--- veterinaria/veterinaria_api/urls.py
+++ veterinaria/veterinaria_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PropietarioViewSet, PacienteViewSet, CitaViewSet
+from .views import *
 
 router = DefaultRouter()
 router.register(r'duenos', PropietarioViewSet)
 router.register(r'mascotas', PacienteViewSet)
 router.register(r'citas', CitaViewSet)
+router.register(r'desparacitaciones', ConsultaViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
