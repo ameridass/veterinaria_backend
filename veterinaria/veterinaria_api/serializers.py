0aeddfbfc65ccfc43f34ce453d0f078f586ff821
asosa <asosa@gtinfile.onmicrosoft.com>
2024-04-17 13:58:16 -0600

    feat
    Serializador para citas.

--- veterinaria/veterinaria_api/serializers.py
+++ veterinaria/veterinaria_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Propietario, Paciente
+from .models import Propietario, Paciente, Cita
 
 
 class PropietarioSerializer(serializers.ModelSerializer):
@@ -11,4 +11,10 @@ class PropietarioSerializer(serializers.ModelSerializer):
 class PacienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Paciente
+        fields = '__all__'
+
+
+class CitaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cita
         fields = '__all__'
