b048e414d19db414dccdeb8bf2fe77016170ed53
asosa <asosa@gtinfile.onmicrosoft.com>
2024-04-17 13:13:35 -0600

    feat
    Ruteo de controlador Paciente (Mascotas)

--- veterinaria/veterinaria_api/urls.py
+++ veterinaria/veterinaria_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PropietarioViewSet
+from .views import PropietarioViewSet, PacienteViewSet
 
 router = DefaultRouter()
 router.register(r'duenos', PropietarioViewSet)
+router.register(r'mascotas', PacienteViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
