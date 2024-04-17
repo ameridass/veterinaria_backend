2081f2f4f7160535f1b2728bd533c9b8f61cefb0
asosa <asosa@gtinfile.onmicrosoft.com>
2024-04-17 13:12:44 -0600

    feat
    Se agrega controlador para Paciente (Mascota)

--- veterinaria/veterinaria_api/views.py
+++ veterinaria/veterinaria_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
-from .models import Propietario
-from .serializers import PropietarioSerializer
+from .models import Propietario, Paciente
+from .serializers import PropietarioSerializer, PacienteSerializer
 
 
 class PropietarioViewSet(viewsets.ModelViewSet):
     queryset = Propietario.objects.all()
     serializer_class = PropietarioSerializer
+
+
+class PacienteViewSet(viewsets.ModelViewSet):
+    queryset = Paciente.objects.all()
+    serializer_class = PacienteSerializer
