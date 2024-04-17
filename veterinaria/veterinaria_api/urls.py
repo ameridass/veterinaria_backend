629e660a575ac20c9079867ad1366146551d1b91
asosa <asosa@gtinfile.onmicrosoft.com>
2024-04-17 13:58:42 -0600

    feat
    Ruteo de citas.

--- veterinaria/veterinaria_api/urls.py
+++ veterinaria/veterinaria_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PropietarioViewSet, PacienteViewSet
+from .views import PropietarioViewSet, PacienteViewSet, CitaViewSet
 
 router = DefaultRouter()
 router.register(r'duenos', PropietarioViewSet)
 router.register(r'mascotas', PacienteViewSet)
+router.register(r'citas', CitaViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
